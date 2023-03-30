f99cc931c9522f4e96a5275f02096ba1119e2b67
mahmodaldahol010@gmail.com <mahmodaldahol010@gmail.com>
2023-03-30 08:49:27 -0700

    add activity and user profile sections

--- base/views.py
+++ base/views.py
@@ -66,25 +66,52 @@ def home(request):
     )
     room_count = rooms.count()
     topics = Topic.objects.all()
-    context = {"rooms": rooms, "topics": topics, "room_count": room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+
+    context = {
+        "rooms": rooms,
+        "topics": topics,
+        "room_count": room_count,
+        "room_messages": room_messages,
+    }
     return render(request, "base/home.html", context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created') # _set for get set of child objects [message]
+    room_messages = (
+        room.message_set.all()
+    )  # _set for get set of child objects [message]
     participants = room.participants.all()
-    if request.method == 'POST':
+    if request.method == "POST":
         message = Message.objects.create(
-            user=request.user,
-            room=room,
-            body=request.POST.get('body')
+            user=request.user, room=room, body=request.POST.get("body")
         )
         room.participants.add(request.user)
-        return redirect('room', pk=room.id)
-    context = {"room": room, "room_messages": room_messages, "participants": participants}
+        return redirect("room", pk=room.id)
+    context = {
+        "room": room,
+        "room_messages": room_messages,
+        "participants": participants,
+    }
 
     return render(request, "base/room.html", context)
+
+
+def user_profile(request, pk):
+    user = User.objects.get(id=pk)
+    rooms = user.room_set.all()
+    room_messages = (
+        user.message_set.all()
+    )  # _set for get set of child objects [message]
+    topics = Topic.objects.all()
+    context = {
+        "user": user,
+        "rooms": rooms,
+        "room_messages": room_messages,
+        "topics": topics,
+    }
+    return render(request, "base/profile.html", context)
 
 
 @login_required(login_url="login")
